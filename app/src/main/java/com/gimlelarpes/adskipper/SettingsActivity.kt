60f07a28df93f000cfdc6609a9797039c22258f5
Gimle Larpes <gimlelarpes@gmail.com>
2024-11-12 17:40:11 +0100

    worked on splashscreen

--- app/src/main/java/com/gimlelarpes/adskipper/SettingsActivity.kt
+++ app/src/main/java/com/gimlelarpes/adskipper/SettingsActivity.kt
@@ -5,18 +5,23 @@ import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.gimlelarpes.adskipper.ui.theme.AdSkipperTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Splash screen
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {//Screen container
