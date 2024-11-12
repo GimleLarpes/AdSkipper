8b9ef26ece3ecebd2479f34c99c8aee5da04cb25
Gimle Larpes <gimlelarpes@gmail.com>
2024-11-12 01:03:44 +0100

    added files

--- app/src/main/java/com/gimlelarpes/adskipper/MainActivity.kt
+++ app/src/main/java/com/gimlelarpes/adskipper/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContent {//Screen container
             AdSkipperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
