7517128ca01a26a7e3c4425d84173499cba1d51e
Gimle Larpes <gimlelarpes@gmail.com>
2024-11-12 02:48:09 +0100

    worked on typography

--- app/src/main/java/com/gimlelarpes/adskipper/MainActivity.kt
+++ app/src/main/java/com/gimlelarpes/adskipper/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.gimlelarpes.adskipper
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -24,10 +26,6 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                    Licenses(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
@@ -36,13 +34,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-@Composable
-fun Licenses(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
